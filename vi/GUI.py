8ad8fa66ff604429c6f54c4040055299a58c8c52
husainsaqib <husainsaqib14@gmail.com>
2025-08-23 18:27:10 +0530

    changes

--- vi/GUI.py
+++ vi/GUI.py
@@ -17,13 +17,19 @@ text_label= Label(frame,text="MosaqAI-Assistant",font=("comic Sans MS",14,"bold"
 text_label.grid(row=0,column=0,padx=20,pady=10)
 
 #image
-image=ImageTk.PhotoImage(Image.open("assistant.jpg"))
-image_label=Label(root,image=image)
+image=ImageTk.PhotoImage(Image.open("image/sed.jpeg"))
+image_label=Label(frame,image=image)
 image_label.grid(row=1,column=0,pady=20)
 
+#adding A Text widget
+text=Text(root,font=('courier 10 bold'),bg="#356696")
+text.grid(row=2,column=0)
+text.place(x=10,y=300,height=90)
 
+#adding entry widgit
 
-
+entry=Entry(root,justify=CENTER)
+entry.place(x=100,y=450,width=350,height=30)
 
 
 root.tk.mainloop()
